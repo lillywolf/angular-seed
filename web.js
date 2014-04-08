a175a3b4accf0b0175452250869bc699b6775ea6
lillywolf <lillywolf@gmail.com>
2014-04-08 13:53:00 -0400

    updated web.js

--- web.js
+++ web.js
@@ -2,16 +2,26 @@
 var http = require('http');
 
 var static = require('node-static');
+var express = require('express');
+var logfmt = require("logfmt");
+
+var app = express();
 
 // Configure our HTTP server to respond with Hello World to all requests.
-var file = new(static.Server)('app/');
-var server = http.createServer(function (request, response) {
-  file.serve(request, response);
+// var file = new(static.Server)('app/');
+// var server = http.createServer(function (request, response) {
+//   file.serve(request, response);
+// });
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
 var port = Number(process.env.PORT || 5000);
 
 // Listen on port
-server.listen(port, function() {
+app.listen(port, function() {
   console.log("Listening on " + port);
 });
