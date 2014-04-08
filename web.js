35cbd951eb52fc4df337e7f03d8e9efd53db7149
lillywolf <lillywolf@gmail.com>
2014-04-08 13:54:30 -0400

    updated web.js

--- web.js
+++ web.js
@@ -15,13 +15,14 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.get('app/', function(req, res) {
+  // res.send('Hello World!');
+	file.serve(req, res);
 });
 
 var port = Number(process.env.PORT || 5000);
 
 // Listen on port
 app.listen(port, function() {
-  console.log("Listening on " + port);
+   console.log("Listening on " + port);
 });
