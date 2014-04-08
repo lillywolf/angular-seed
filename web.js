0d392d7b752fcb0e430ba9d65e73dc3b1d20f570
lillywolf <lillywolf@gmail.com>
2014-04-08 14:07:14 -0400

    updated web.js

--- web.js
+++ web.js
@@ -15,10 +15,12 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  	res.send('Hello World!');
-	//file.serve(req, res);
-});
+// app.get('/', function(req, res) {
+//   	res.send('Hello World!');
+// 		file.serve(req, res);
+// });
+
+app.use('/app', app.router);
 
 var port = Number(process.env.PORT || 5000);
 
