f0e7068f12c99c55f179be79689efbbccd0a449f
lillywolf <lillywolf@gmail.com>
2014-04-09 12:53:33 -0400

    added captions

--- app/js/instagram.js
+++ app/js/instagram.js
@@ -1,7 +1,7 @@
 
 function InstaBoxer() {
 	this.instaData = {};	
-	this.accessToken = "268284987.c171e68.116f34f427994b90acd1b2b3701367f0";
+	this.accessToken = "268284987.c171e68.116f34f427994b90acd1b2b3701367f0"; // To do: get this from a database
 	this.imageUrls = [];
 		
 	this.initInstagrams = function(callback) {
@@ -22,8 +22,10 @@ function InstaBoxer() {
 	this.formatInstagrams = function(callback) {
 		var me = this;
 		this.instaData.data.forEach(function(entry) {
+			// console.log(entry);
 			me.imageUrls.push({
 				url: entry.images.standard_resolution.url, 
+				caption: entry.caption.text,
 				class: 'w' + (Math.round(Math.random() * 2)).toString()});
 		});
 		callback(this.imageUrls);
--- app/js/services.js
+++ app/js/services.js
@@ -2,9 +2,6 @@
 
 /* Services */
 
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
 angular.module('lillyBoxes.services', [])
 	.value('version', '0.1')
 	.factory('instaGet', ['$q', '$rootScope', 'instaBoxer', function($q, $rootScope, instaBoxer) {
